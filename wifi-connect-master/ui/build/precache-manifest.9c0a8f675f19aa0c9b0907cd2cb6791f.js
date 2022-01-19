8642a6c887b5ed02642505cf4fb0850de4f0826a
Robin Louis Schmidt <schmidt.robin.louis@gmail.com>
2022-01-19 19:13:51 +0100

    surcemap fix

---
+++ wifi-connect-master/ui/build/precache-manifest.9c0a8f675f19aa0c9b0907cd2cb6791f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3ff3a9f0b71630e8a61cdd3cb1cc33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3421f29ccbabda004a21",
+    "url": "/static/css/2.c38cd9da.chunk.css"
+  },
+  {
+    "revision": "3421f29ccbabda004a21",
+    "url": "/static/js/2.ba3dcd11.chunk.js"
+  },
+  {
+    "revision": "28ce00ad444b69bea42850e06eec9da3",
+    "url": "/static/js/2.ba3dcd11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba43768fcc3ba6f7c632",
+    "url": "/static/js/main.a044aed1.chunk.js"
+  },
+  {
+    "revision": "0e903404fe668039b1a7",
+    "url": "/static/js/runtime-main.2a78626f.js"
+  },
+  {
+    "revision": "795fa8e65c8fa6fa7000b314c84933ef",
+    "url": "/static/media/logo.795fa8e6.png"
+  }
+]);
